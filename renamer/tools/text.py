2bb9e559330c7676844aa24df257ddb6a4ace282
4seal <85556739+4seal@users.noreply.github.com>
2021-10-11 03:12:37 +0300

    Update text.py

--- renamer/tools/text.py
+++ renamer/tools/text.py
@@ -22,11 +22,11 @@ class TEXT:
 
 ** Developer:** {bot_owner}
 
-** Channel:** [NS BOT UPDATES](https://t.me/Ns_bot_updates)
+** Channel:** [👾HACKERS BASH👾](https://t.me/hackersbash)
 
-** Group:** [Ns BOT SUPPORT](https://t.me/Ns_Bot_supporters)
+** Group:** [👾HACKERS BASH👾](https://t.me/hackersbash)
 
-** Source Code:** [Press Me](https://github.com/Ns-AnoNymouS/TG-RENAMER-BOT)
+** Source Code:** [Press Me](https://t.me/kippikbot)
 """
 
     HELP_USER = """**Follow Below Steps:**
@@ -59,4 +59,4 @@ __--Payment Methods:--__
  
 GooglePay / Paytm / PhonPay / paypal / Net Banking
  
-**For Donate:** message me @MaxxRiderz"""
+**For Donate:** message me @deprokip"""
